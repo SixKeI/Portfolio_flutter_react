0a146d2b49fa5153d509eb08125b6e4c2ec10718
SixKeI <133827299+SixKeI@users.noreply.github.com>
2026-04-17 20:53:11 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
-connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-// routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/projects", require("./routes/projects"));
-app.use("/api/contact", require("./routes/contact"));
+// DB
+connectDB();
 
-app.listen(3000, () => {
-  console.log("Backend running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Backend running on", PORT);
 });
